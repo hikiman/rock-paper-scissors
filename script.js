7692b0b287cd6a82fdbda40194fed37ab969df76
Aleksei Denisov <tech.denisov@gmail.com>
2025-02-09 19:45:13 +0400

    refactor: put code for creating buttons into playGame function

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const body = document.querySelector("body");
-const buttonSelectRock = document.createElement("button");
-const buttonSelectPaper = document.createElement("button");
-const buttonSelectScissors = document.createElement("button");
-
-buttonSelectRock.textContent = "Choose Rock";
-buttonSelectPaper.textContent = "Choose Paper";
-buttonSelectScissors.textContent = "Choose Scissors";
-
-body.append(buttonSelectRock, buttonSelectPaper, buttonSelectScissors);
-
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3 + 1);
 
@@ -42,6 +31,17 @@ function getHumanChoice() {
 }
 
 function playGame() {
+  const body = document.querySelector("body");
+  const buttonSelectRock = document.createElement("button");
+  const buttonSelectPaper = document.createElement("button");
+  const buttonSelectScissors = document.createElement("button");
+
+  buttonSelectRock.textContent = "Choose Rock";
+  buttonSelectPaper.textContent = "Choose Paper";
+  buttonSelectScissors.textContent = "Choose Scissors";
+  
+  body.append(buttonSelectRock, buttonSelectPaper, buttonSelectScissors);
+  
   let humanScore = 0,
     computerScore = 0,
     loopCounter = 1,
